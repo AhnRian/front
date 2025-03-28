31ebcb07d87d3fb2be6437aa81b8ae7eafa9c961
AhnRian <00rian@naver.com>
2025-03-28 17:15:40 +0000

    [FIX]: 배포 오류 해결 (#183)
    * 🐛 fix: 얼굴 등록 이후 등록 완료 실시간 렌더링 변경
    
    * 🐛 fix: 배포 오류

--- src/pages/user/FaceRegistration.tsx
+++ src/pages/user/FaceRegistration.tsx
@@ -4,7 +4,6 @@ import * as S from './FaceRegistration.style';
 import Webcam from 'react-webcam';
 import { faceRegister } from '../../api/face/faceRegister';
 import { Toast } from '../../components/common/toast/Toast';
-import { useNavigate } from 'react-router-dom';
 import { faceMsg, ToastState } from '../../constant/faceMsg';
 import Loading from '../../components/common/loading/Loading';
 
@@ -13,7 +12,6 @@ const FaceRegistration = () => {
   const [hasCaptured, setHasCaptured] = useState(false);
   const [faceState, setFaceState] = useState<ToastState>('default');
   const [isCameraVisible, setIsCameraVisible] = useState(true);
-  const navigate = useNavigate();
 
   const handleFaceDetected = (captureImage: () => void) => {
     if (!hasCaptured) {
@@ -47,7 +45,13 @@ const FaceRegistration = () => {
   }, [hasCaptured]);
 
   useEffect(() => {
-    if (!isCameraVisible) navigate('/profile');
+    if (!isCameraVisible) {
+      const video = webcamRef.current?.video;
+      const stream = video?.srcObject as MediaStream;
+      stream?.getTracks().forEach((track) => track.stop());
+
+      window.location.href = '/profile';
+    }
   }, [isCameraVisible]);
 
   const { isLoading, isFaceInside, isVideoLoaded, capturedImage } =
